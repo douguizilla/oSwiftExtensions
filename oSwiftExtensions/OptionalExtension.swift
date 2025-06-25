50e5ed91083d58667ff9ea750124545510cab0c8
Douglas Gomes <douglasgommez@gmail.com>
2025-06-25 20:04:52 -0300

    feat: add ifNil, ifNotNil, let, isNil, isNotNil.

--- oSwiftExtensions/OptionalExtension.swift
+++ oSwiftExtensions/OptionalExtension.swift
@@ -8,10 +8,28 @@
 import Foundation
 
 extension Optional {
-    func ifNil(otherwise: () -> Wrapped) -> Wrapped {
+    func ifNil(_ otherwise: () -> Wrapped) -> Wrapped {
         guard let value = self else {
             return otherwise()
         }
         return value
+    }
+    
+    func ifNotNil<T>(_ closure: ((Wrapped) -> T?)) -> T? {
+        guard let v = self else { return nil }
+        return closure(v)
+    }
+    
+    func `let`(do: (Wrapped)->()){
+        guard let value = self else { return }
+        `do`(value)
+    }
+    
+    var isNil: Bool {
+        return self == nil
+    }
+    
+    var isNotNil: Bool {
+        return self != nil
     }
 }
