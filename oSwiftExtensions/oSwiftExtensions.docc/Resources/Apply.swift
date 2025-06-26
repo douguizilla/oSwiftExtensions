6ca5fa67d08fbd1a2bbc1f90fc53ace039e36147
Douglas Gomes <douglasgommez@gmail.com>
2025-06-25 21:47:15 -0300

    feat: add Apply. Alteração do typealias.

---
+++ oSwiftExtensions/oSwiftExtensions.docc/Resources/Apply.swift
@@ -0,0 +1,14 @@
+//
+//  Apply.swift
+//  
+//
+//  Created by Douglas Gomes on 25/06/25.
+//
+
+protocol Apply {}
+
+extension Apply {
+    func apply(closure: (Self) -> ()) -> Void {
+        closure(self)
+    }
+}
