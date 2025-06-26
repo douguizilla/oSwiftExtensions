daa3e3fbcbcee834b4525c14a36f3b8588bdf4bc
Douglas Gomes <douglasgommez@gmail.com>
2025-06-26 00:21:26 -0300

    refact: alinhamento.

--- oSwiftTools/oSwiftTools.docc/Extensions/SequenceExtension.swift
+++ oSwiftTools/oSwiftTools.docc/Extensions/SequenceExtension.swift
@@ -8,8 +8,12 @@
 import Foundation
 
 extension Sequence {
-    func groupedBy<Key: Hashable>(keyForValue: (Element) -> Key) -> Dictionary {
-        let groupedDictionary = self.reduce(into: [Key: [Element]]()) { dictionary, element in
+    func groupedBy<Key: Hashable>(
+        keyForValue: (Element) -> Key
+    ) -> Dictionary {
+        let groupedDictionary = self.reduce(
+            into: [Key: [Element]]()
+        ) { dictionary, element in
             let key = keyForValue(element)
             dictionary[key, default: []].append(element)
         }
